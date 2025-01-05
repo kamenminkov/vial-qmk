1370fa4bbe85ca4b46bde2fc89f4381b6609a652
Kamen Minkov <kamen.minkov@gmail.com>
2025-01-05 19:14:20 +0200

    WIP

--- keyboards/kradoindustries/promenade/keymaps/kamenminkov/keymap.c
+++ keyboards/kradoindustries/promenade/keymaps/kamenminkov/keymap.c
@@ -12,7 +12,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
 #include "action_layer.h"
 #include "keycodes.h"
@@ -33,7 +32,7 @@ enum layer_names {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWE] = LAYOUT(
+    [_QWE] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_F5,   KC_F12,  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
