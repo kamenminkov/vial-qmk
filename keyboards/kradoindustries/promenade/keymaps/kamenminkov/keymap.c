39b141f9853b2a4c20b8192320cfff02a58e2fe6
Kamen Minkov <kamen.minkov@gmail.com>
2025-09-19 12:13:03 +0300

    WIP

--- keyboards/kradoindustries/promenade/keymaps/kamenminkov/keymap.c
+++ keyboards/kradoindustries/promenade/keymaps/kamenminkov/keymap.c
@@ -29,36 +29,38 @@ enum layer_names {
 #define ADJ MO(_ADJ)
 #define CADL LCA(KC_DEL)
 #define PIPE LSFT(KC_BSLS)
+#define LPAREN LSFT(KC_9)
+#define RPAREN LSFT(KC_0)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWE] = LAYOUT_all(
+    [_QWE] = LAYOUT_2x2u(
         KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_F5,   KC_F12,  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_DEL,  KC_BSLS, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-        KC_LCTL, KC_LGUI, NAV,     KC_LALT,  NAV,     KC_SPC,  KC_SPC,      KC_ENT,      KC_BSPC, NUM,     PIPE,    KC_BSLS, KC_RCTL
+        KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    LPAREN,  RPAREN , KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
+        KC_LCTL, KC_LGUI, NAV,     KC_LALT,  NAV,              KC_SPC,      KC_ENT,      KC_BSPC, NUM,     PIPE,    KC_BSLS, KC_RCTL
     ),
-    [_NAV] = LAYOUT_all(
+    [_NAV] = LAYOUT_2x2u(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F12,
         _______, _______, KC_MPLY, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,
         _______, _______, _______, KC_DEL,  _______, _______,                   KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
         _______, KC_MENU, _______, _______, _______, _______, _______, _______, CADL,    _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, RGB_TOG
+        _______, _______, _______, _______, _______,          _______,     _______,      _______, _______, _______, _______, RGB_TOG
     ),
-    [_NUM] = LAYOUT_all(
+    [_NUM] = LAYOUT_2x2u(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,                   KC_BSPC, KC_4,    KC_5,    KC_6,    _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,     _______,      KC_0,    _______, _______, _______, _______
+        _______, _______, _______, _______, _______,          _______,     _______,      KC_0,    _______, _______, _______, _______
     ),
-    [_ADJ] = LAYOUT_all(
+    [_ADJ] = LAYOUT_2x2u(
         QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                   _______, RGB_SAD, RGB_VAD, RGB_SAI, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, RGB_TOG,     RGB_TOG,      _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______,          RGB_TOG,     RGB_TOG,      _______, _______, _______, _______, _______
     ),
 };
 // clang-format on
